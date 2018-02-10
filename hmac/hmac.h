add1ad073df22d8c691190156e2d98fb1a6f5a76
Zer1t0 <Zer1t0@hotmail.com>
2018-02-10 15:49:15 +0100

    rfc reference included

--- hmac/hmac.h
+++ hmac/hmac.h
@@ -3,6 +3,8 @@
 #include <Windows.h>
 #include "md5.h"
 
+// RFC: https://tools.ietf.org/html/rfc2104
+
 typedef DWORD(*hashFunction)(PBYTE, DWORD64, PBYTE*);
 
 DWORD hmac(PBYTE key, DWORD64 keyLen, PBYTE message, DWORD64 messageLen, hashFunction hashFunc, USHORT blockSize, USHORT digestSize, PBYTE *digest);
